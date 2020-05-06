9bf2852c5c5f5e1909915ee0654eb3919d21a1a2
LambdaXymox <lambda.xymox@gmail.com>
2020-05-05 21:06:04 -0500

    Add scaffolding for rendering the triangle.

--- src/main.rs
+++ src/main.rs
@@ -29,12 +29,18 @@ mod gl {
 }
 
 mod gl_backend;
+mod mesh;
 
 use gl_backend as glh;
+use mesh::Mesh;
+
 use glfw::{Action, Context, Key};
 use gl::types::{GLfloat, GLint, GLuint, GLvoid, GLsizeiptr};
 use log::{info};
 use teximage2d::TexImage2D;
+
+use std::io;
+use std::ptr;
 
 
 const CLEAR_COLOR: [f32; 4] = [0.2_f32, 0.2_f32, 0.2_f32, 1.0_f32];
@@ -44,26 +50,57 @@ const GL_TEXTURE_MAX_ANISOTROPY_EXT: u32 = 0x84FE;
 const GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT: u32 = 0x84FF;
 
 
-/// Initialize the logger.
-fn init_logger(log_file: &str) {
-    file_logger::init(log_file).expect("Failed to initialize logger.");
+/// Load texture image into the GPU.
+fn send_to_gpu_texture(image: &TexImage2D, wrapping_mode: GLuint) -> Result<GLuint, String> {
+    let mut tex = 0;
+    unsafe {
+        gl::GenTextures(1, &mut tex);
+    }
+    debug_assert!(tex > 0);
+    unsafe {
+        gl::ActiveTexture(gl::TEXTURE0);
+        gl::BindTexture(gl::TEXTURE_2D, tex);
+        gl::TexImage2D(
+            gl::TEXTURE_2D, 0, gl::RGBA as i32, image.width as i32, image.height as i32, 0,
+            gl::RGBA, gl::UNSIGNED_BYTE,
+            image.as_ptr() as *const GLvoid
+        );
+        gl::GenerateMipmap(gl::TEXTURE_2D);
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_S, wrapping_mode as GLint);
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_WRAP_T, wrapping_mode as GLint);
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MAG_FILTER, gl::LINEAR as GLint);
+        gl::TexParameteri(gl::TEXTURE_2D, gl::TEXTURE_MIN_FILTER, gl::LINEAR_MIPMAP_LINEAR as GLint);
+    }
+
+    let mut max_aniso = 0.0;
+    unsafe {
+        gl::GetFloatv(GL_MAX_TEXTURE_MAX_ANISOTROPY_EXT, &mut max_aniso);
+        // Set the maximum!
+        gl::TexParameterf(gl::TEXTURE_2D, GL_TEXTURE_MAX_ANISOTROPY_EXT, max_aniso);
+    }
+
+    Ok(tex)
 }
 
-/// Create and OpenGL context.
-fn init_gl(width: u32, height: u32) -> glh::GLState {
-    let gl_state = match glh::start_gl(width, height) {
-        Ok(val) => val,
-        Err(e) => {
-            panic!("Failed to Initialize OpenGL context. Got error: {}", e);
-        }
-    };
-
-    gl_state
+#[derive(Copy, Clone)]
+struct ShaderSource {
+    vert_name: &'static str,
+    vert_source: &'static str,
+    frag_name: &'static str,
+    frag_source: &'static str,
 }
 
-struct Mesh {
-    points: [[f32; 2]; 3],
-    tex_coords: [[f32; 2]; 3],
+fn send_to_gpu_shaders(game: &mut glh::GLState, source: ShaderSource) -> GLuint {
+    let mut vert_reader = io::Cursor::new(source.vert_source);
+    let mut frag_reader = io::Cursor::new(source.frag_source);
+    let sp = glh::create_program_from_reader(
+        game,
+        &mut vert_reader, source.vert_name,
+        &mut frag_reader, source.frag_name
+    ).unwrap();
+    debug_assert!(sp > 0);
+
+    sp
 }
 
 fn create_meshj_triangle(h: f32) -> Mesh {
@@ -79,18 +116,7 @@ fn create_meshj_triangle(h: f32) -> Mesh {
         [0_f32, 0_f32], [1_f32, 0_f32], [0.5, 1_f32],
     ];
 
-    Mesh {
-        points: points,
-        tex_coords: tex_coords,
-    }
-}
-
-#[derive(Copy, Clone)]
-struct ShaderSource {
-    vert_name: &'static str,
-    vert_source: &'static str,
-    frag_name: &'static str,
-    frag_source: &'static str,
+    Mesh::new(&points, &tex_coords)
 }
 
 fn create_shaders_triangle() -> ShaderSource {
@@ -108,6 +134,111 @@ fn create_shaders_triangle() -> ShaderSource {
 fn create_textures_triangle() -> TexImage2D {
     let asset = include_bytes!("../assets/marble.png");
     teximage2d::load_from_memory(asset).unwrap().image
+}
+
+#[derive(Copy, Clone)]
+struct Buffers {
+    vao: GLuint,
+    v_pos_vbo: GLuint,
+    v_tex_vbo: GLuint,
+    v_pos_loc: GLuint,
+    v_tex_loc: GLuint,
+}
+
+fn create_buffers_triangle(sp: GLuint) -> Buffers {
+    let v_pos_loc = unsafe {
+        gl::GetAttribLocation(sp, glh::gl_str("v_pos").as_ptr())
+    };
+    debug_assert!(v_pos_loc > -1);
+    let v_pos_loc = v_pos_loc as GLuint;
+    
+    let v_tex_loc = unsafe {
+        gl::GetAttribLocation(sp, glh::gl_str("v_tex").as_ptr())
+    };
+    debug_assert!(v_tex_loc > -1);
+    let v_tex_loc = v_tex_loc as GLuint;
+
+    let mut v_pos_vbo = 0;
+    unsafe {
+        gl::GenBuffers(1, &mut v_pos_vbo);
+    }
+    debug_assert!(v_pos_vbo > 0);
+
+    let mut v_tex_vbo = 0;
+    unsafe {
+        gl::GenBuffers(1, &mut v_tex_vbo);
+    }
+    debug_assert!(v_tex_vbo > 0);
+
+    let mut vao = 0;
+    unsafe {
+        gl::GenVertexArrays(1, &mut vao);
+    }
+    debug_assert!(vao > 0);
+    unsafe {
+        gl::BindVertexArray(vao);
+        gl::BindBuffer(gl::ARRAY_BUFFER, v_pos_vbo);
+        gl::VertexAttribPointer(v_pos_loc, 2, gl::FLOAT, gl::FALSE, 0, ptr::null());
+        gl::BindBuffer(gl::ARRAY_BUFFER, v_tex_vbo);
+        gl::VertexAttribPointer(v_tex_loc, 2, gl::FLOAT, gl::FALSE, 0, ptr::null());
+        gl::EnableVertexAttribArray(v_pos_loc);
+        gl::EnableVertexAttribArray(v_tex_loc);
+    }
+
+    Buffers {
+        vao: vao,
+        v_pos_vbo: v_pos_vbo,
+        v_tex_vbo: v_tex_vbo,
+        v_pos_loc: v_pos_loc,
+        v_tex_loc: v_tex_loc,
+    }
+}
+
+fn send_to_gpu_geometry_background(handle: Buffers, mesh: &Mesh) {
+    unsafe {
+        // Load position data.
+        gl::BindBuffer(gl::ARRAY_BUFFER, handle.v_pos_vbo);
+        gl::BufferData(
+            gl::ARRAY_BUFFER,
+            mesh.points.len_bytes() as GLsizeiptr,
+            mesh.points.as_ptr() as *const GLvoid, gl::STATIC_DRAW
+        );
+        // Load the texture coordinates.
+        gl::BindBuffer(gl::ARRAY_BUFFER, handle.v_tex_vbo);
+        gl::BufferData(
+            gl::ARRAY_BUFFER,
+            mesh.tex_coords.len_bytes() as GLsizeiptr,
+            mesh.tex_coords.as_ptr() as *const GLvoid, gl::STATIC_DRAW
+        );
+
+        // Enable the arrays for use by the shader.
+        gl::BindVertexArray(handle.vao);
+        gl::BindBuffer(gl::ARRAY_BUFFER, handle.v_pos_vbo);
+        gl::VertexAttribPointer(handle.v_pos_loc, 2, gl::FLOAT, gl::FALSE, 0, ptr::null());
+        gl::BindBuffer(gl::ARRAY_BUFFER, handle.v_tex_vbo);
+        gl::VertexAttribPointer(handle.v_tex_loc, 2, gl::FLOAT, gl::FALSE, 0, ptr::null());
+        gl::EnableVertexAttribArray(handle.v_pos_loc);
+        gl::EnableVertexAttribArray(handle.v_tex_loc);
+    }
+}
+
+
+
+/// Initialize the logger.
+fn init_logger(log_file: &str) {
+    file_logger::init(log_file).expect("Failed to initialize logger.");
+}
+
+/// Create and OpenGL context.
+fn init_gl(width: u32, height: u32) -> glh::GLState {
+    let gl_state = match glh::start_gl(width, height) {
+        Ok(val) => val,
+        Err(e) => {
+            panic!("Failed to Initialize OpenGL context. Got error: {}", e);
+        }
+    };
+
+    gl_state
 }
 
 fn main() {
---
+++ src/mesh.rs
@@ -0,0 +1,106 @@
+/*
+ *  Googly Blocks is a video game.
+ *  Copyright (C) 2018,2019,2020  Christopher Blanchard
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+use std::mem;
+
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct Points {
+    inner: Vec<[f32; 2]>,
+}
+
+impl Points {
+    #[inline]
+    pub fn as_ptr(&self) -> *const [f32; 2] {
+        self.inner.as_ptr()
+    }
+
+    /// Get the length of the points buffer in bytes.
+    #[inline]
+    pub fn len_bytes(&self) -> usize {
+        3 * mem::size_of::<f32>() * self.inner.len()
+    }
+
+    /// Get the number of elements in the points buffer.
+    #[inline]
+    pub fn len(&self) -> usize {
+        self.inner.len()
+    }
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub struct TextureCoordinates {
+    inner: Vec<[f32; 2]>,
+}
+
+impl TextureCoordinates {
+    #[inline]
+    pub fn as_ptr(&self) -> *const [f32; 2] {
+        self.inner.as_ptr()
+    }
+
+    /// Get the length of the texture coordinates buffer in bytes.
+    #[inline]
+    pub fn len_bytes(&self) -> usize {
+        2 * mem::size_of::<f32>() * self.inner.len()
+    }
+
+    /// Get the number of elements in the texture coordinates buffer.
+    #[inline]
+    pub fn len(&self) -> usize {
+        self.inner.len()
+    }
+}
+
+/// An `ObjMesh` is a model space representation of a 2D geometric figure.
+#[derive(Clone, Debug, PartialEq)]
+pub struct Mesh {
+    pub points: Points,
+    pub tex_coords: TextureCoordinates,
+}
+
+impl Mesh {
+    /// Generate a new mesh object.
+    pub fn new(points: &[[f32; 2]], tex_coords: &[[f32; 2]]) -> Mesh {
+        Mesh {
+            points: Points { inner: points.iter().map(|e| *e).collect() },
+            tex_coords: TextureCoordinates { inner: tex_coords.iter().map(|e| *e).collect() },
+        }
+    }
+
+    /// Present the points map as an array slice. This function can be used
+    /// to present the internal array buffer to OpenGL or another Graphics
+    /// system for rendering.
+    #[inline]
+    pub fn points(&self) -> &[[f32; 2]] {
+        &self.points.inner
+    }
+
+    /// Present the texture map as an array slice. This function can be used
+    /// to present the internal array buffer to OpenGL or another Graphics
+    /// system for rendering.
+    #[inline]
+    pub fn tex_coords(&self) -> &[[f32; 2]] {
+        &self.tex_coords.inner
+    }
+
+    /// Get the number of vertices in the mesh.
+    #[inline]
+    pub fn len(&self) -> usize {
+        self.points.len()
+    }
+}
